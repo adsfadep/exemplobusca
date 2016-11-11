87f68bd5a3f6a108a3254b2c34d1b9109752af02
Diego Occhi <diegocchi@gmail.com>
2016-11-11 21:30:39 -0200

    lerarquivo

--- js/webServiceCtrl.js
+++ js/webServiceCtrl.js
@@ -7,6 +7,7 @@ angular.module('webServiceApp').controller('webServiceCtrl', function($scope, $h
 
 	$scope.ip = 'localhost:8080';
 	$scope.servicoSomar = '/HelloWorldWs/ws/teste/somar';
+	$scope.servicoLerArquivo = '/HelloWorldWs/ws/servicos/lerArquivo';
 
 	$scope.somar = function(num1, num2) {
 		$http.get('http://' + $scope.ip + $scope.servicoSomar, {params:{num1:num1,num2:num2}})
@@ -15,4 +16,10 @@ angular.module('webServiceApp').controller('webServiceCtrl', function($scope, $h
 			});
 	}
 
+	$scope.lerArquivo = function() {
+		$http.get('http://' + $scope.ip + $scope.servicoLerArquivo)
+			.success(function(resultado) {
+				$scope.conteudoArquivo = resultado;
+			});
+	}
 });
